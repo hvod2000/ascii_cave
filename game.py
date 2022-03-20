14f08e5b4d1c01065ba76885e7c00908f59a6fc0
Uladzislau Khamkou <kham@tuta.io>
2022-03-20 17:54:08 +0300

    improve readability of the map format

--- game.py
+++ game.py
@@ -41,11 +41,14 @@ def read_graph_from_binary(source):
 
 
 def read_graph_from_edges(source):
-    cave = source.split("\n")
+    cave = source.strip().split("\n")
     n = int(cave[0])
-    lvl_graph = {i: set() for i in range(n)}
-    for u in range(n):
-        for v in map(int, cave[u + 1].strip().split()):
+    lvl_graph = {}
+    for line in map(lambda s: s.strip(), cave[1:]):
+        u, vs = line.split(":")
+        u = int(u)
+        lvl_graph[u] = set()
+        for v in (int(v) for v in vs.strip().split(" ") if v):
             lvl_graph[u].add(v)
     lvls = LevelsTopology.from_available_moves(lvl_graph)
     return Cave.from_levels_topology(lvls)
@@ -68,12 +71,12 @@ player = random_player_position(cave)
 done = False
 while not done:
     print("\n" * 100)
-    layer = layer2str(cave, player[2], lambda i: str(i + 1))
+    layer = layer2str(cave, player[2])
     add_noise_to_image(layer)
     layer[~player[1]][player[0]] = "@"
     print("  GET TO FINISH ROOM #1!")
     print("\t" + "\n\t".join("".join(row) for row in layer))
-    if isinstance(cave[player], int) and cave.items[cave[player]] == 0:
+    if isinstance(cave[player], int) and cave.items[cave[player]] == 1:
         print()
         print("CONGRATUATIONS ON YOUR HAED!")
         print("YOU WON!")
